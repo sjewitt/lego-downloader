685f2292e7664d898274f14eaf19ccc5a2dfb339
sjewitt <6630247+sjewitt@users.noreply.github.com>
2022-12-13 15:24:57 +0000

    Add files via upload

--- static/js/engine.js
+++ static/js/engine.js
@@ -27,6 +27,15 @@ var engine = {
     		 *     engine.getplandata({'show':nnnnnn});
     		 * for getting a set directly from source
     		 */
+    		 
+    		 /** paginated endpoints */
+    		 $('#startlinks > li').each(function(){
+			console.log('appending handler to ', this)
+				$(this).on('click',function(){
+					console.log(this);
+					engine.getplandata_paginated({'page_length':25, 'filter': $(this).attr('data-action')});
+				});
+			 })
     	}
     },
     
@@ -91,13 +100,13 @@ var engine = {
             	if(engine.planData[a].SetNumber !== undefined){
             		
 	            	var _desc = engine.planData[a].Description;
-	            	var _notes = engine.planData[a].Notes;
+	            	//var _notes = engine.planData[a].Notes;
 	            	if(_desc.length === 0){
 	            		_desc = "[No description]";
 	            	}
-	            	if(_notes.length === 0){
+	            	/*if(_notes.length === 0){
 	            		_notes = "[No notes]";
-	            	}
+	            	}*/
 	
 	            	//detect the download status form the object. The downloaded flag will be checked at the python end.
 	            	var _isFlaggedForDownload = engine.planData[a].download;
@@ -122,7 +131,7 @@ var engine = {
 	            	_out += '<tr data-plan-item="' + engine.planData[a].key + '"><td>' 
 	            		+ engine.planData[a].SetNumber+'</td><td class="plan-handler" data-plan-field="Description"><span>' 
 	            		+ _desc + '</span></td><td class="plan-handler" data-plan-field="Notes"><span>' 
-	            		+ _notes 
+	            		//+ _notes 
 	            		+ '</span></td><td class="download_checkbox"><span class="plan_add"><input type="checkbox" value="' 
 	            		+ engine.planData[a].key + '" ' + _downloadFlagIsChecked + '></span></td>'
 	            		+ '<td>'+_isDownloadedMsg+'</td>'
@@ -161,6 +170,183 @@ var engine = {
         });
     },
     
+    
+
+    getplandata_paginated : function(params){
+    	let page_length=25;
+    	let curr_page = 0;
+    	let filter = 'all'
+    	if(params['page_length']){
+			page_length = parseInt(params['page_length']);
+		}
+		if(params['page_num']){
+			curr_page = parseInt(params['page_num']);
+		}
+		if(params['filter']){
+			filter = params['filter'];
+		}
+    	/** need page_length, curr_page and filter */
+    	$.ajax({
+            type: "GET",
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            url : "/api/paginated_plandata?page_length=" + page_length + "&page_num=" + curr_page + "&filter=" + filter
+        }).done(function(data){
+			console.log("/api/paginated_plandata?page_length=" + page_length + "&page_num=" + curr_page + "&filter=" + filter)
+        	engine.planData = data;
+        	// build as DOM properly
+        	var _out = "<table id='plans_listingxxx'><thead><tr><th>Set number</th><th>Description</th><th>Notes</th><th>Actions</th><th></th></tr></thead>";
+        	_out += "<tfoot><th>Set number</th><th>Description</th><th>Notes</th><th>Actions</th><th></th></tfoot><tbody>";
+        	
+        	//TODO: Exclude items that are flagged as unavailable (i.e. we have alrady tried to get the plan, but we got a response othter than 200OK) 
+            for(var a=0;a<engine.planData.entries.length;a++){
+            	if(engine.planData.entries[a].SetNumber !== undefined){
+	            	var _desc = engine.planData.entries[a].Description;
+	            	if(_desc.length === 0){
+	            		_desc = "[No description]";
+	            	}
+
+	            	//detect the download status form the object. The downloaded flag will be checked at the python end.
+	            	var _isFlaggedForDownload = engine.planData.entries[a].download;
+	            	var _isDownloaded = engine.planData.entries[a].downloaded;
+	            	
+	            	var _downloadFlagIsChecked = '';
+	            	if(_isFlaggedForDownload){
+	            		_downloadFlagIsChecked = 'checked="checked"';
+	            	}
+	            	if(_isDownloaded){
+	            		_downloadFlagIsChecked = 'disabled="disabled"';
+	            	}
+	            	
+	            	/*Do as DOM elements - faster. Also - do some timing tests...*/
+	            	var _isDownloadedMsg = '';
+	            	if(_isDownloaded){
+	            		_isDownloadedMsg = '[<a href="/api/getstoredplan?getlocal=' + engine.planData.entries[a].key + '.pdf" target="_blank">open</a>]'; 
+	            		_isDownloadedMsg += ' [<a href="/api/getstoredplan?action=download&getlocal=' + engine.planData.entries[a].key + '.pdf">download</a>]'
+	            		_isDownloadedMsg += ' [<span class="link" data-action="reset">Reset</span>]'  ///api/resetdownload
+	            	}
+	            	
+	            	_out += '<tr data-plan-item="' + engine.planData.entries[a].key + '"><td>' 
+	            		+ engine.planData.entries[a].SetNumber+'</td><td class="plan-handler" data-plan-field="Description"><span>' 
+	            		+ _desc + '</span></td><td class="plan-handler" data-plan-field="Notes"><span>' 
+	            		+ '</span></td><td class="download_checkbox"><span class="plan_add"><input type="checkbox" value="' 
+	            		+ engine.planData.entries[a].key + '" ' + _downloadFlagIsChecked + '></span></td>'
+	            		+ '<td>'+_isDownloadedMsg+'</td>'
+	            		+ '</tr>';
+            	}
+            }
+            _out += '</table>';
+            $('#plans_list').html(_out);
+            $('#plans_listing').dataTable({
+            	  "pageLength": 50
+            });
+            
+            //see SO #30794672 - defer binding so that hidden elements are also flagged:
+            $('table').on('click','td.download_checkbox',function(){
+
+            	//send AJAX call to back end for this
+            	engine.setDownloadFlag($(this).find('input').attr('value'),$(this).find('input').is(':checked'));
+            });
+            
+            $('table').on('click','td.plan-handler > span',function(){
+            	//generate a form element, with a blur handler to reset it (removing the input, re-showing the text with update? TO CONFIRM)
+            	engine.getEditField($(this));
+            });
+            
+            $('table').on('click','span.link',function(){
+            	//generate a form element, with a blur handler to reset it (removing the input, re-showing the text with update? TO CONFIRM)
+            	if($(this).attr('data-action') === "reset"){
+            		engine.resetDownload($(this).parent().parent().attr('data-plan-item'));
+            	}
+            	//need to remove  the parent tr as well
+            	$(this).parent().parent().remove()
+            });  
+            
+            //paginator:
+            let _paginator_location = document.getElementById('paginator') ;
+            _paginator_location.innerHTML = "" 
+            _paginator_location.appendChild(engine.getPaginationLinks(data.pagination_data))  
+        }).fail(function(jqxhr, status, e){ 
+            console.log("err"); 
+        });
+    },
+    
+    getPaginationLinks : function(data){
+		console.log(data)
+		let total_pages = Math.ceil(data.total / data.page_length);
+		
+		let _wrapper = document.createElement('ul');
+		
+		let _back_many = document.createElement('li')
+		_back_many.appendChild(document.createTextNode('<<'));
+		let _back_one = document.createElement('li')
+		_back_one.appendChild(document.createTextNode('<'));
+		
+		let _pagination_info = document.createElement('li')
+		
+		let _fwd_one = document.createElement('li')
+		_fwd_one.appendChild(document.createTextNode('>'));
+		let _fwd_many = document.createElement('li')
+		_fwd_many.appendChild(document.createTextNode('>>'));
+		
+		let paginationHandler = function(){
+			let args = {
+				page_num:parseInt(this.getAttribute('data-targetpage')),
+				filter:this.getAttribute('data-filter'),
+				page_length:this.getAttribute('data-page_length'),
+				total:this.getAttribute('data-total')
+			}
+			console.log(args)
+			engine.getplandata_paginated(args);
+		}
+		
+//		let stats = document.getElementById('paginator_stats');
+		_pagination_info.innerHTML = 'page ' + data.curr_page + ' of ' + total_pages;
+		console.log(data.curr_page, total_pages)
+		
+		let prevpagemany = data.curr_page > 0 ? data.curr_page - 15 : 0;
+		let prevpageone = data.curr_page > 0 ? data.curr_page-1 : 0;
+		let nextpageone = data.curr_page <= total_pages ? data.curr_page+1 :  total_pages; 
+		let nextpagemany = data.curr_page <= total_pages ? data.curr_page + 15 :  total_pages;
+		
+		_back_one.setAttribute('data-targetpage',prevpageone);
+		_back_many.setAttribute('data-targetpage',prevpagemany);
+		_fwd_one.setAttribute('data-targetpage',nextpageone);
+		_fwd_many.setAttribute('data-targetpage',nextpagemany);
+		
+		_back_one.setAttribute('data-total',data.total);
+		_back_many.setAttribute('data-total',data.total);
+		_fwd_one.setAttribute('data-total',data.total);
+		_fwd_many.setAttribute('data-total',data.total);
+		
+		_back_one.setAttribute('data-page_length',data.page_length);
+		_back_many.setAttribute('data-page_length',data.page_length);
+		_fwd_one.setAttribute('data-page_length',data.page_length);
+		_fwd_many.setAttribute('data-page_length',data.page_length);
+		
+		_back_one.setAttribute('class','paginator');
+		_back_many.setAttribute('class','paginator');
+		_fwd_one.setAttribute('class','paginator');
+		_fwd_many.setAttribute('class','paginator');
+		
+		_back_one.setAttribute('data-filter',data.filter_key);
+		_back_many.setAttribute('data-filter',data.filter_key);
+		_fwd_one.setAttribute('data-filter',data.filter_key);
+		_fwd_many.setAttribute('data-filter',data.filter_key);
+		
+		_back_one.addEventListener('click',paginationHandler);
+		_back_many.addEventListener('click',paginationHandler);
+		_fwd_one.addEventListener('click',paginationHandler);
+		_fwd_many.addEventListener('click',paginationHandler);
+		
+		_wrapper.appendChild(_back_many);
+		_wrapper.appendChild(_back_one);
+		_wrapper.appendChild(_pagination_info);
+		_wrapper.appendChild(_fwd_one);
+		_wrapper.appendChild(_fwd_many);
+		return(_wrapper);
+	},
+
     resetDownload : function(key){
     	var _params = {'key':key};
     	$.ajax({
@@ -170,8 +356,6 @@ var engine = {
             data:JSON.stringify(_params),
             url : "/api/resetdownload"
         }).done(function(data){ });
-    	
-    	
     },
     
     setDownloadFlag : function(setnumber,flag){
